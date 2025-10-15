2b3664d2d519e2dd08544a47145efebd659c9a50
noscode <ekaterina.e.noskova@gmail.com>
2025-10-15 20:49:13 +0100

    Add tutorial

---
+++ 2025-10-GADMA_Worshop/tutorials/4_GADMA_tutorial/outputs/gadma_outputs/gadma_output_2epochs/7/final_best_logLL_model_dadi_code.py
@@ -0,0 +1,34 @@
+import dadi
+import numpy as np
+
+def model_func(params, ns, pts):
+	t1, nu11 = params
+	_Nanc_size = 1.0  # This value can be used in splits with fractions
+	xx = dadi.Numerics.default_grid(pts)
+	phi = dadi.PhiManip.phi_1D(xx)
+	nu1_func = lambda t: _Nanc_size + (nu11 - _Nanc_size) * (t / t1)
+	phi = dadi.Integration.one_pop(phi, xx, T=t1, nu=nu1_func)
+	sfs = dadi.Spectrum.from_phi(phi, ns, [xx]*len(ns))
+	return sfs
+
+data = dadi.Spectrum.from_file('/home/enoskova/Workspace/GADMA_workshops/2025-10-GADMA_Worshop/tutorials/3_easySFS_tutorial/outputs/easySFS_output/dadi/NN-14.sfs')
+pts = [20, 30, 40]
+ns = data.sample_sizes
+
+p0 = [0.06326230573267977, 0.02688200391432464]
+lower_bound = [1e-15, 0.01]
+upper_bound = [5.0, 100.0]
+func_ex = dadi.Numerics.make_extrap_log_func(model_func)
+model = func_ex(p0, ns, pts)
+ll_model = dadi.Inference.ll_multinom(model, data)
+print('Model log likelihood (LL(model, data)): {0}'.format(ll_model))
+
+theta = dadi.Inference.optimal_sfs_scaling(model, data)
+print('Optimal value of theta: {0}'.format(theta))
+
+Nanc = 339627.92577896116
+mu = 1.554e-08
+L = 10000000
+theta0 = 4 * mu * L
+Nanc = int(theta / theta0)
+print('Size of ancestral population: {0}'.format(Nanc))
